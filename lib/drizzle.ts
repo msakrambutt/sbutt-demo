780a567bdbbfa1edfca05d39eed693ab0ec95713
bhaiwork <bhaiwork22plus@gmail.com>
2023-06-27 15:32:47 +0500

    updates

--- lib/drizzle.ts
+++ lib/drizzle.ts
@@ -20,8 +20,32 @@ export const usertable = pgTable("users", {
       }).notNull().default("'user'"),
   });
 
+  export const playlistTable = pgTable("playlist", {
+    id: serial("id").primaryKey(),
+    user_id: integer("user_id").notNull().references(() => usertable.id),
+    order_date: date("order_date").notNull(),
+    course_id: varchar("course_id", {
+      length: 255,
+    }).notNull(),
+  });
+  
+  export const watchTimeTable = pgTable("watch_time", {
+    id: serial("id").primaryKey(),
+    playlist_id: integer("playlist_id").notNull().references(() => playlistTable.id),
+    watch_video_no: integer("watch_video_no").notNull(),
+    watch_video_id: varchar("watch_video_id", {
+      length: 255,
+    }).notNull(),
+  });
+
 export type Users=InferModel<typeof usertable >;
 export type NewUsers=InferModel<typeof usertable,"insert" >;
+
+export type Playlist = InferModel<typeof playlistTable>;
+export type NewPlaylist = InferModel<typeof playlistTable, "insert">;
+
+export type WatchTime = InferModel<typeof watchTimeTable>;
+export type NewWatchTime = InferModel<typeof watchTimeTable, "insert">;
 
 
 export const db = drizzle(sql);
